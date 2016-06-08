a75294aca07b4857f0753f64e73c74ad17bec939
Leo Adelstein <leoadelstein@gmail.com>
2016-06-08 16:23:22 -0700

    implemented basic text and image form

---
+++ components/App.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import AddListing from './AddListing.jsx';
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+  render() {
+    return (<div> APP.JSX
+      <AddListing />
+    </div>);
+  }
+}
+
+export default App;
