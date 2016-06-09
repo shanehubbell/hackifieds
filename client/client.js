18e0a3a343a3774084c0a07ac0f32f6375013dc0
Tai Huynh <anhtaih89@gmail.com>
2016-06-08 21:19:38 -0700

    Start working on Single Listing component and add some css styling

--- client/client.js
+++ client/client.js
@@ -5,6 +5,7 @@ import { render } from 'react-dom';
 import App from './components/App.jsx';
 import Listings from './components/Listings.jsx';
 import AddListing from './components/AddListing.jsx';
+import SingleListing from './components/SingleListing.jsx';
 
 import { Router, Route } from 'react-router';
 import store, { history } from './redux/store';
@@ -14,8 +15,9 @@ const router = (
   <Provider store={store}>
     <Router history={history}>
       <Route path="/" component={App} >
-        <Route path="/listings" component={Listings} />
+        <Route path="/all-listings" component={Listings} />
         <Route path="/add-listings" component={AddListing} />
+        <Route path="/single-listings" component={SingleListing} />
       </Route>
     </Router>
   </Provider>
---
+++ client/components/SingleListing.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const SingleListing = () => (
+  <div>
+    <hr />
+    <img
+      alt="House"
+      src="https://i.ytimg.com/vi/92G8nB4yhGQ/maxresdefault.jpg"
+    />
+    <hr />
+    <p>
+      <span className="fa fa-usd" aria-hidden="true">3000/month</span>
+      <br />
+      <span className="fa fa-bed" aria-hidden="true">1 Bed</span>
+    </p>
+
+    <h1>Summary Points</h1>
+  </div>
+);
+
+export default SingleListing;
