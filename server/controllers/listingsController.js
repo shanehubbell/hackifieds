1cc750b65c3d229653f22b74f1f3ce7293cb202d
Leo Adelstein <leoadelstein@gmail.com>
2016-06-09 15:28:10 -0700

    form text/img post endpoint

--- server/controllers/listingsController.js
+++ server/controllers/listingsController.js
@@ -19,11 +19,10 @@ module.exports = {
   },
   addListing: function addListing(req, res) {
     console.log(req.body);
-    db.Listing.create({ userId: '123', address: 'greenhaven', lat: '45',
-      lng: '44', distanceToHackReactor: '66', price: '$22.22',
-      bathrooms: '2', private: false, ownerEmail: 'james@gmail.com',
-      ownerName: 'James Marker', pictures: JSON.stringify(['ppp.jpg', 'img.png']),
-      description: 'funfun' })
+    db.Listing.create({ address: req.body.address, price: req.body.price,
+      bathrooms: req.body.bathrooms, private: JSON.parse(req.body.private),
+      ownerEmail: req.body.ownerEmail, ownerName: req.body.ownerName,
+      description: req.body.description, pictures: req.body.pictures })
       .then((response) => {
         res.end(JSON.stringify(response.dataValues));
       })
@@ -33,4 +32,3 @@ module.exports = {
       });
   },
 };
-
