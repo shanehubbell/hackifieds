2533e8884b567698320766f7c7a23c376090cf34
Tai Huynh <anhtaih89@gmail.com>
2016-06-08 19:36:15 -0700

    Test routing code

--- client/components/Main.jsx
+++ client/components/Main.jsx
@@ -1,6 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router';
 
+const Main = (props) => (
+  <div>
+    <h1>
+      <Link to="/">HackB2B</Link><br />
+      <Link to="/listings">Listings</Link><br />
+      <Link to="/add-listings">Add Listings</Link><br />
+    </h1>
+    {props.children}
+  </div>
+);
+
+Main.propTypes = {
+  children: React.PropTypes.object,
+};
+
+export default Main;
 
 // class App extends Component {
 
@@ -16,19 +32,3 @@ import { Link } from 'react-router';
 //     </div>);
 //   }
 // }
-
-const Main = (props) => (
-  <div>
-    <h1>
-      <Link to="/">HackB2B</Link>
-      <Link to="/listings">Listings</Link>
-    </h1>
-    {props.children}
-  </div>
-);
-
-Main.propTypes = {
-  children: React.PropTypes.object,
-};
-
-export default Main;
