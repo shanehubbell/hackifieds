de55ddacc957e5be77dc8693eceb18ec65a56e9b
Leo Adelstein <leoadelstein@gmail.com>
2016-06-09 14:43:19 -0700

    cleaned up the server

--- server/server.js
+++ server/server.js
@@ -1,25 +1,11 @@
 const express = require('express');
 
-// auth
-const passport = require('passport');
-const Strategy = require('passport-github2').Strategy;
-
 // connect to mongoose
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/hackPictures');
 
-// webpack / hmre
-const config = require('../webpack.config.js');
-const webpack = require('webpack');
-const webpackDevMiddleware = require('webpack-dev-middleware');
-const webpackHotMiddleware = require('webpack-hot-middleware');
-const compiler = webpack(config);
 
 const app = express();
-
-app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output.publicPath }));
-app.use(webpackHotMiddleware(compiler));
-
 
 require('./config/middleware.js')(app, express);
 require('./config/routes.js')(app, express);
