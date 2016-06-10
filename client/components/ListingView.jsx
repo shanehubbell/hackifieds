126e39790d813aa2349c4200b5bbe12a0088b9a6
Shane Hubbell <shane.hubbell@gmail.com>
2016-06-09 20:12:50 -0700

    adding some CSS to listing view

--- client/components/ListingView.jsx
+++ client/components/ListingView.jsx
@@ -2,20 +2,50 @@ import React from 'react';
 import ImageView from './imageView.jsx';
 import { connect } from 'react-redux';
 
+// const id = this.props.params.id;
+
+const test = {
+  listings: {
+    1: {
+      images: ['http://www.3dmodelfree.com/imguploads/Image/0905/models_z/0625%20%203D/15.jpg',
+      'http://tny.im/4E8',
+      'http://www.crazy3dfree.com/uploads/091209/1_223952_1.jpg'],
+      description: 'Old, shitty',
+      address: '45 Broad Street',
+      distanceFromHR: '1 mile',
+      crimeRate: '5',
+      owner: 'Marcus',
+      ownerEmail: 'marcus@hackreactor.com',
+    },
+  },
+};
+
+const cssTag = `pure-u-1 pure-u-md-1-${test.listings[1].images.length} pure-img`;
+
 const ListingsView = (props) => (
   <div>
     <div className="pure-g">
-      {Object.keys(this.props.listings[id].images).map((image) =>
-        (<ImageView image={image} />)
+      {test.listings[1].images.map((link) => (
+        <div className={cssTag}>
+          <ImageView image={link} />
+        </div>)
       )}
     </div>
-    <h1>Description: {this.props.listings[id].description}</h1>
-    <h2>Address: {this.props.listings[id].address}</h2>
-    <p>Bathrooms: {this.props.listings[id].address}</p>
-    <p>Distance from HR: {this.props.listings[id].distanceFromHR}</p>
-    <p>Crime Rate: {this.props.listings[id].crimeRate}</p>
-    <p>Owner: {this.props.listings[id].ownerName}</p>
-    <p>Owner Email: {this.props.listings[id].ownerEmail}</p>
+    <h1>{test.listings[1].description}</h1>
+    <h2>Address: {test.listings[1].address}</h2>
+    <p>Bathrooms: {test.listings[1].address}</p>
+    <p>Distance from HR: {test.listings[1].distanceFromHR}</p>
+    <div className="pure-g">
+      <div className="pure-u-1 pure-u-md-1-3">
+        <p>Crime Rate: {test.listings[1].crimeRate}</p>
+      </div>
+      <div className="pure-u-1 pure-u-md-1-3">
+        <p>Owner: {test.listings[1].ownerName}</p>
+      </div>
+      <div className="pure-u-1 pure-u-md-1-3">
+        <p>Owner Email: {test.listings[1].ownerEmail}</p>
+      </div>
+    </div>
   </div>
 );
 
--- client/components/imageView.jsx
+++ client/components/imageView.jsx
@@ -3,11 +3,11 @@ import React from 'react';
 // need the number of images, so we can
 // replace XXX in className below
 const imageView = (props) => (
-  <div className="pure-u-1 pure-u-md-1-XXX pure-img">
-    <img
-		src={props.listing.listingId.image}
-    />
-  </div>
+  <img
+    src={props.image}
+    alt
+    key={props.image}
+  />
 );
 
 export default imageView;
