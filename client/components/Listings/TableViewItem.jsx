50d25a4b9b2d46d5d4e67bb47a9903e67c060f83
Tai Huynh <anhtaih89@gmail.com>
2016-06-09 21:54:31 -0700

    Fix parsing of stringified pictures array back to array on client side and update table view item to reflect

--- client/components/Listings/TableViewItem.jsx
+++ client/components/Listings/TableViewItem.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const TableViewItem = (props) => (
   <div className="pure-g tableViewItem" key={props.listing.listingId} onClick={props.onClick}>
     <div className="pure-u-1-3">
-      <img className="table-image" src={`images/${props.listing.pictures.slice(2, 34)}`} />
+      <img className="table-image" src={`images/${props.listing.pictures[0]}`} />
       <div className="icons">
         <h5><span className="fa fa-usd" aria-hidden="true" />{" "}{props.listing.price}/month</h5>
         <h5><span className="fa fa-compass" aria-hidden="true" />{" "}miles to HR</h5>
