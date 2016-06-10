c1a9b9a5233008049efa8ca3e2213a1ef96dadbd
Shane Hubbell <shane.hubbell@gmail.com>
2016-06-10 15:59:32 -0700

    edited clientjs  to resolve conflicts with master

---
+++ client/components/ListingView.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+// import ImageSlider from './ImageSlider.jsx';
+
+const ListingsView = () => (
+  <div>
+    <div id="Slider">
+      <div class="pure-g">
+        <div class="pure-u-1 pure-u-md-1-3"><img src="http://japanpropertycentral.com/wp-content/uploads/2010/10/asakusa-tower-model-room-medium.jpeg" alt /></div>
+        <div class="pure-u-1 pure-u-md-1-3"><img src="http://www.3dmodelfree.com/imguploads/Image/0905/models_z/0625%20%203D/15.jpg" alt /></div>
+        <div class="pure-u-1 pure-u-md-1-3"><img src="http://homes365.co/wp-content/uploads/2013/06/modern-kitchen-model.jpg" alt /></div>
+      </div>
+    </div>
+    <h1>Listing View</h1>
+    <h1>Listing Title!</h1>
+    <p>Listing Description</p>
+    <p>$2,000</p>
+    <p>July 24th, 2016</p>
+    <p>September 25, 2016</p>
+  </div>
+);
+
+export default ListingsView;
+    // <ImageSlider />
+    // <div>
+    //   <h1>{this.props.title}</h1>
+    //   <p>{this.props.description}</p>
+    // </div>
+    // <div>
+    //   <p>{this.props.price}</p>
+    //   <p>{this.props.startDate}</p>
+    //   <p>{this.props.endDate}</p>
+    // </div>
