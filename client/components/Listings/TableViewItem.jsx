5b63bcf1c6ac225502bd73ce38d3bde3f13e2fa8
Tai Huynh <anhtaih89@gmail.com>
2016-06-09 22:53:18 -0700

    Style more and work on centering tableview

--- client/components/Listings/TableViewItem.jsx
+++ client/components/Listings/TableViewItem.jsx
@@ -2,13 +2,16 @@ import React from 'react';
 
 const TableViewItem = (props) => (
   <div className="pure-g tableViewItem" key={props.listing.listingId} onClick={props.onClick}>
-    <div className="pure-u-1-3">
-      <img className="table-image" src={`images/${props.listing.pictures[0]}`} />
-      <div className="icons">
-        <h5><span className="fa fa-usd" aria-hidden="true" />{" "}{props.listing.price}/month</h5>
-        <h5><span className="fa fa-compass" aria-hidden="true" />{" "}miles to HR</h5>
-        <h5><span className="fa fa-map-marker" aria-hidden="true" />{" "}{props.listing.address}</h5>
-        <h5><span className="fa fa-bed" aria-hidden="true" />{" "}{props.listing.bathrooms  + (props.listing.bathrooms  > 1 ? ' bathrooms' : ' bathroom')}</h5>
+    <div className="pure-g">
+      <img className="table-image pure-u-1-3" src={`images/${props.listing.pictures[0]}`} />
+    </div>
+    <div className="pure-g">
+      <div className="icons pure-u-1-3">
+        <p><span className="fa fa-usd" aria-hidden="true" />{" "}{props.listing.price}</p>
+        <p><span className="fa fa-compass" aria-hidden="true" />{" "}5 miles to HR</p>
+        <p><span className="fa fa-map-marker" aria-hidden="true" />{" "}{props.listing.address}</p>
+        <p><span className={props.listing.private ? "fa fa-user" : "fa fa-users"} aria-hidden="true" />{" "}{props.listing.private ? 'Private Room' : 'Shared Room'}</p>
+        <p><span className="fa fa-bed" aria-hidden="true" />{" "}{props.listing.bathrooms + (props.listing.bathrooms  > 1 ? ' bathrooms' : ' bathroom')}</p>
       </div>
     </div>
   </div>
