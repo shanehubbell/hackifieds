daa705bb4bf13a524fc333c6e49f267d759ec114
Leo Adelstein <leoadelstein@gmail.com>
2016-06-10 20:58:44 -0700

    refactored app.js

--- server/app.js
+++ server/app.js
@@ -1,10 +1,5 @@
 const express = require('express');
 
-// connect to mongoose
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/hackPictures');
-
-
 const app = express();
 
 require('./config/middleware.js')(app, express);
