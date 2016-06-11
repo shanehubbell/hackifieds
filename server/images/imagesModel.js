6073565b856491e41cb16abaead95bb4cdb8a9fe
Leo Adelstein <leoadelstein@gmail.com>
2016-06-10 21:00:25 -0700

    deleted old files

--- server/images/imagesModel.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-const imagesSchema = mongoose.Schema({
-  img: { data: Buffer, contentType: String },
-});
-
-module.exports = mongoose.model('Image', imagesSchema);
